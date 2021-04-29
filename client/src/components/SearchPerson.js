f773f43bfb2cc8eb3efa34dc81240c359fdc39f3
Solpapaya <dsolorioparedes@gmail.com>
2021-04-29 15:44:37 -0500

    Seach Input Validation Completed
    In the search input you can't type blank spaces at the beginning of the string, you can't add double blank spaces between words. Also, the search request to the server is not made if the only thing that was iserted by the user was a blank space. There is a drawback, when there is no match in the database and the user keeps typing, requests to the server are still made. A good approach could be preventing sending more requests if we already know there is no match with the first string the user typed

--- client/src/components/SearchPerson.js
+++ client/src/components/SearchPerson.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef, useEffect } from "react";
 import { PeopleContext } from "../context/PeopleContext";
 import { SearchContext } from "../context/SearchContext";
 import { fetchData } from "../functions/fetchData";
@@ -7,19 +7,47 @@ const SearchPerson = () => {
   const [search, setSearch] = useState("");
   const { setIsSearchSuccessful } = useContext(SearchContext);
   const { setPeople } = useContext(PeopleContext);
+  const searchRef = useRef(null);
 
   const onChangeHandler = async (e) => {
-    setSearch(e.target.value);
-    let url = `/people?search=${e.target.value}`;
-    try {
-      const response = await fetchData("get", url);
-      setPeople(response.data.people);
-      setIsSearchSuccessful(true);
-    } catch (err) {
-      setPeople([]);
-      setIsSearchSuccessful(false);
+    // Prevent blank spaces at the beginning of the search
+    let search = e.target.value.trimLeft();
+    if (
+      search[search.length - 1] === " " &&
+      search[search.length - 2] === " "
+    ) {
+      // Prevent Double Blank Space between words
+      search = search.substring(0, search.length - 1);
+    }
+
+    setSearch(search);
+
+    if (e.target.value !== " " && search[search.length - 1] !== " ") {
+      search = search.trim();
+      // Preventing search with more than 4 words
+      if (search.split(" ").length < 5) {
+        console.log("Searching person");
+        const url = `/people?search=${search}`;
+        try {
+          const response = await fetchData("get", url);
+          setPeople(response.data.people);
+          setIsSearchSuccessful(true);
+        } catch (err) {
+          setPeople([]);
+          setIsSearchSuccessful(false);
+        }
+      } else {
+        // Tell user is not allowed search with more than 4 words
+        // console.log("No more than 4 words");
+        // Remove the 5th word
+        setSearch(search.split(" ").slice(0, 4).join(" "));
+      }
     }
   };
+
+  useEffect(() => {
+    console.log(searchRef.current.focus());
+  }, []);
 
   return (
     <div className="mb-4 text-center">
@@ -27,6 +55,7 @@ const SearchPerson = () => {
         type="text"
         value={search}
         onChange={onChangeHandler}
+        ref={searchRef}
         placeholder="Nombre o RFC"
       />
     </div>
