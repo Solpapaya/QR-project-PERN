a5780858d7491d1340791bf53c9cc00ef674d1f5
Daniel Rafael Solorio Paredes <solpapaya@Daniels-MacBook-Pro.local>
2021-06-15 18:21:14 -0500

    All tables can be exported to CSV
    And they have an option for telling the user that an empty table can not be exported, the message for advising the user is still missing

--- client/src/components/PeopleList.js
+++ client/src/components/PeopleList.js
@@ -2,6 +2,11 @@ import React, { useContext, useEffect } from "react";
 import { PeopleContext } from "../context/PeopleContext";
 import { SearchContext } from "../context/SearchContext";
 import PeopleTable from "./PeopleTable";
+import { ExportBtnContext } from "../context/ExportBtnContext";
+import { SearchSubsectionContext } from "../context/SearchSubsectionContext";
+
+import * as FileSaver from "file-saver";
+import * as XLSX from "xlsx";
 
 const PeopleList = () => {
   const {
@@ -11,7 +16,103 @@ const PeopleList = () => {
     setIsSearchSuccessful,
     people,
     setFilteredPeople,
+    setStatusFilter,
+    setDepartmentFilter,
   } = useContext(SearchContext);
+
+  const { exportBtn } = useContext(ExportBtnContext);
+  const { searchSection } = useContext(SearchSubsectionContext);
+
+  const exportToCSV = () => {
+    let people;
+    let status;
+    let department;
+    // For getting the latest value of the States
+    setFilteredPeople((prevPeople) => {
+      people = prevPeople;
+      return prevPeople;
+    });
+
+    if (people.length > 0) {
+      setStatusFilter((prevStatus) => {
+        status = prevStatus;
+        return prevStatus;
+      });
+      setDepartmentFilter((prevDepartment) => {
+        department = prevDepartment;
+        return prevDepartment;
+      });
+      exportData(people, status, department);
+    } else {
+      // Alert user the table is empty and can not an empty table
+      console.log("Table is empty");
+    }
+  };
+
+  const exportData = (people, status, department) => {
+    const fileType =
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+    const fileExtension = ".xlsx";
+    // Adds the titles of each column like 'First Name' or 'RFC' , etc
+    const exportTable = addHeaderToTable(people);
+    const ws = XLSX.utils.json_to_sheet(exportTable, {
+      skipHeader: true,
+    });
+    // Defines the width of each column
+    ws["!cols"] = [
+      { width: 15 },
+      { width: 20 },
+      { width: 20 },
+      { width: 20 },
+      { width: 20 },
+      { width: 20 },
+      { width: 15 },
+    ];
+    // Sets the file name depending on the filters applied to the Data
+    // If the table only shows active people, the file name should be
+    // 'Person_Table_Active' or if the people shown in the browser are
+    // people inactive from the Finance department, the name should be
+    // 'Person_Table_Disabled_Finance'
+    const fileName = setExportFileName(status, department);
+    const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+    const data = new Blob([excelBuffer], { type: fileType });
+    FileSaver.saveAs(data, fileName + fileExtension);
+  };
+
+  const setExportFileName = (status, department) => {
+    let fileName = "Tabla_Personas";
+
+    if (status === "active") fileName += "_Activas";
+    else if (status === "disabled") fileName += "_Inactivas";
+
+    if (department === "null") fileName += "_SinAsignar";
+    else if (department !== "all_areas")
+      fileName += `_${department.replace(" ", "")}`;
+    return fileName;
+  };
+
+  const addHeaderToTable = (obj) => {
+    const header = {
+      first_name: "Primer Nombre",
+      second_name: "Segundo Nombre",
+      surname: "Primer Apellido",
+      second_surname: "Segundo Apellido",
+      rfc: "RFC",
+      department_name: "Área",
+      creation_date: "Fecha de Alta",
+      active: "Estado",
+    };
+    // Changes values true and false for 'Active' or 'Disabled'
+    const newObj = obj.map((person) => {
+      const newRow = { ...person };
+      if (newRow.active) newRow.active = "Activo";
+      else newRow.active = "Inactivo";
+      return { ...newRow };
+    });
+    const exportTable = [header, ...newObj];
+    return exportTable;
+  };
 
   const filterPeople = () => {
     let newPeople;
@@ -44,6 +145,14 @@ const PeopleList = () => {
   useEffect(() => {
     filterPeople();
   }, [people, statusFilter, departmentFilter]);
+
+  useEffect(() => {
+    // Code for resetting click events listeners in Export Button
+    const new_element = exportBtn.current.cloneNode(true);
+    exportBtn.current.parentNode.replaceChild(new_element, exportBtn.current);
+    new_element.addEventListener("click", exportToCSV);
+    exportBtn.current = new_element;
+  }, [searchSection]);
 
   return (
     <div className="table-container">
--- client/src/components/StatusLogsList.js
+++ client/src/components/StatusLogsList.js
@@ -2,6 +2,11 @@ import React, { useEffect, useContext } from "react";
 import { MonthsContext } from "../context/MonthsContext";
 import { SearchStatusLogsContext } from "../context/SearchStatusLogsContext";
 import StatusLogsTable from "./StatusLogsTable";
+import { ExportBtnContext } from "../context/ExportBtnContext";
+import { SearchSubsectionContext } from "../context/SearchSubsectionContext";
+
+import * as FileSaver from "file-saver";
+import * as XLSX from "xlsx";
 
 const StatusLogsList = () => {
   const {
@@ -12,9 +17,103 @@ const StatusLogsList = () => {
     monthFilter,
     statusFilter,
     setFilteredStatusLogs,
+    setYearFilter,
+    setMonthFilter,
+    setStatusFilter,
   } = useContext(SearchStatusLogsContext);
 
   const { months } = useContext(MonthsContext);
+  const { exportBtn } = useContext(ExportBtnContext);
+  const { searchSection } = useContext(SearchSubsectionContext);
+
+  const exportToCSV = () => {
+    let status;
+    let year;
+    let month;
+    let statusFilter;
+
+    // For getting the latest value of the States
+    setFilteredStatusLogs((prevStatus) => {
+      status = prevStatus;
+      return prevStatus;
+    });
+
+    // If the table is not empty
+    if (status.length > 0) {
+      setYearFilter((prevYear) => {
+        year = prevYear;
+        return prevYear;
+      });
+      setMonthFilter((prevMonth) => {
+        month = prevMonth;
+        return prevMonth;
+      });
+      setStatusFilter((prevStatusFilter) => {
+        statusFilter = prevStatusFilter;
+        return prevStatusFilter;
+      });
+
+      exportData(status, year, month, statusFilter);
+    } else {
+      // Alert user the table is empty and can not an empty table
+      console.log("Table is empty");
+    }
+  };
+
+  const exportData = (status, year, month, statusFilter) => {
+    const fileType =
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+    const fileExtension = ".xlsx";
+    // Adds the titles of each column like 'First Name' or 'RFC' , etc
+    const exportTable = addHeaderToTable(status);
+    const ws = XLSX.utils.json_to_sheet(exportTable, {
+      skipHeader: true,
+    });
+    // Defines the width of each column
+    ws["!cols"] = [{ width: 15 }, { width: 20 }, { width: 40 }, { width: 20 }];
+    // Sets the file name depending on the filters applied to the Data
+    // If the table only shows active people, the file name should be
+    // 'Person_Table_Active' or if the people shown in the browser are
+    // people inactive from the Finance department, the name should be
+    // 'Person_Table_Disabled_Finance'
+    const fileName = setExportFileName(year, month, statusFilter);
+    const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+    const data = new Blob([excelBuffer], { type: fileType });
+    FileSaver.saveAs(data, fileName + fileExtension);
+  };
+
+  const setExportFileName = (year, month, statusFilter) => {
+    let fileName = "Tabla_CambiosDeEstado";
+
+    if (year !== "all") fileName += `_${year}`;
+
+    if (month !== "all") fileName += `_${month}`;
+
+    if (statusFilter === "active") fileName += "_Activas";
+    else if (statusFilter === "disabled") fileName += "_Inactivas";
+
+    return fileName;
+  };
+
+  const addHeaderToTable = (obj) => {
+    const header = {
+      date: "Fecha",
+      new_status: "Nuevo Estado",
+      full_name: "Nombre Completo",
+      rfc: "RFC",
+    };
+
+    const newObj = obj.map((tax) => {
+      const newRow = { ...tax, id: null };
+      if (newRow.new_status) newRow.new_status = "Activo";
+      else newRow.new_status = "Inactivo";
+      return newRow;
+    });
+
+    const exportTable = [header, ...newObj];
+    return exportTable;
+  };
 
   const filterStatusLogs = () => {
     let newStatusLogs = [];
@@ -48,6 +147,14 @@ const StatusLogsList = () => {
   useEffect(() => {
     filterStatusLogs();
   }, [yearFilter, monthFilter, statusFilter, statusLogs]);
+
+  useEffect(() => {
+    // Code for resetting click events listeners in Export Button
+    const new_element = exportBtn.current.cloneNode(true);
+    exportBtn.current.parentNode.replaceChild(new_element, exportBtn.current);
+    new_element.addEventListener("click", exportToCSV);
+    exportBtn.current = new_element;
+  }, [searchSection]);
 
   return (
     <div className="table-container">
